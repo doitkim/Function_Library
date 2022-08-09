9369fbcd914eb2f15f316ab17b74a797d25cee11
doitkim <kimsh27214@gmail.com>
2022-08-09 09:35:41 +0900

    ToDoList_모바일버전

---
+++ TodoList/todolist_main.js
@@ -0,0 +1,31 @@
+// To Do List 내용 입력에 필요한 선택자 지정
+const content = document.querySelector('#content');
+
+//지정된 선택자에 리스트 추가 함수 작성
+function addNewTodo() {
+    const li = document.createElement('li');
+    let todolist_index = document.getElementById('todolist_add_text').value;
+    if(todolist_index ===''){
+        alert('입력되지 않았습니다.');
+    }
+    else if (content.children.length > 7){
+        alert('할 일을 추가할 수 없습니다.');
+    }
+    
+    else {
+        li.textContent = todolist_index;
+        content.append(li);
+    }
+    
+    
+}
+
+function removeTodo() {
+    if (content.children[0] === undefined){
+        alert('목록이 없습니다.');
+    }
+    else {
+        content.removeChild(content.children[content.children.length-1]);
+    }
+        
+}
